8ea3da6eaaa5440df5f474057cae18932ca48fc3
unknown <disco2dad@yahoo.com>
2020-08-26 17:08:57 -0600

    Updates

---
+++ precache-manifest.ce3ebab305ecc4984f4ba7e4a960ef5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34760cbe0ebac7b568994e2fdbf93c4",
+    "url": "/MonsterRoldex/index.html"
+  },
+  {
+    "revision": "79d9c78db58b01711c8f",
+    "url": "/MonsterRoldex/static/css/main.cc9a4fee.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MonsterRoldex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRoldex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d9c78db58b01711c8f",
+    "url": "/MonsterRoldex/static/js/main.fb7f9385.chunk.js"
+  },
+  {
+    "revision": "b414663164baee130920",
+    "url": "/MonsterRoldex/static/js/runtime-main.0fce6e5f.js"
+  }
+]);
